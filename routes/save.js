dc2d894d9f28cbb67135c95ffa8b4884882a5f9a
binarytester <110983840+binarytester@users.noreply.github.com>
2023-04-04 10:24:16 -0300

    Create save.js
    adicionando pagina de erros simples

--- routes/save.js
+++ routes/save.js
@@ -11,25 +11,34 @@ router.use(bodyParser.json());
 
 // router to save the transcription of word
 router.post("/text/save", async (req, res) => {
-  let text = req.body.text.toLowerCase();
-  let originalText = text;
-  await gAudio.generateAudio(text);
-  const translation = await reverso.generateTranslate(text);
-  const phrases = await reverso.generatePhrase(text);
-  const urlImage = await openai.generateImage(text);
+  try {
+    let text = req.body.text.toLowerCase().trim();
 
-  // traslating IPA to words
-  text = dictOp.findWord(text);
-  const engWord = dictOp.translateWord(text);
-  res.render("../views/transcripited/newText.ejs", {
-    text: text,
-    audio: "../audio.mp3",
-    engWord: engWord,
-    urlImage: urlImage,
-    phrases: phrases,
-    originalText: originalText,
-    translation: translation,
-  });
+    // generate audio, translation, phrases and image
+    const originalText = text;
+    gAudio.generateAudio(text);
+    const translation = await reverso.generateTranslate(text);
+    const phrases = await reverso.generatePhrase(text);
+    const urlImage = await openai.generateImage(text);
+    console.log(urlImage);
+    // translating IPA to words
+    const engWord = dictOp.translateWord(text);
+
+    res.render("../views/transcripited/newText.ejs", {
+      text: text,
+      audio: "../audio.mp3",
+      engWord: engWord,
+      urlImage: urlImage,
+      phrases: phrases,
+      originalText: originalText,
+      translation: translation,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("../views/error.ejs", {
+      message: "An error occurred while processing your request.",
+    });
+  }
 });
 
 module.exports = router;
